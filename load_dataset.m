cd2c35c249cd4ef9ac0b0d7e50e7178828530a66
joaoamrodrigues <joaoamrodrigues@ua.pt>
2024-12-01 19:16:39 +0000

    dataset processing

---
+++ load_dataset.m
@@ -0,0 +1,16 @@
+function [header,video_IDs,titles,channel_names,categories,tags,descriptions] = load_dataset(filename)
+
+Dataset = readcell(filename);
+header = Dataset(1,:);
+Data = Dataset(2:end,:);
+
+video_IDs = Data(:,1);
+titles = Data(:,3);
+channel_names = Data(:,4);
+categories = Data(:,5);
+tags = Data(:,7);
+descriptions = Data(:,16);
+
+clear Dataset;
+clear Data;
+end
---
+++ untitled.m
@@ -0,0 +1,3 @@
+FILE_NAME = "USvideos_short.csv";
+
+[header,video_IDs,titles,channel_names,categories,tags,descriptions] = load_dataset(FILE_NAME)
