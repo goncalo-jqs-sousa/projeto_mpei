356e5251441dca7b31168822bc6d03b54b7a65f9
joaoamrodrigues <joaoamrodrigues@ua.pt>
2024-12-10 19:52:49 +0000

    Update load_dataset.m

--- load_dataset.m
+++ load_dataset.m
@@ -5,7 +5,7 @@ header = Dataset(1,:);
 Data = Dataset(2:end,:);
 
 video_IDs = Data(:,1);
-n_samples = size(video_IDs,1);
+n_samples = length(video_IDs);
 titles = Data(:,3);
 channel_names = Data(:,4);
 categories = Data(:,5);
