67d96d4599dfb32aa184029daf05b950fae7d6fa
joaoamrodrigues <joaoamrodrigues@ua.pt>
2024-12-15 16:03:08 +0000

    added time

--- main.m
+++ main.m
@@ -7,12 +7,15 @@ Cat = [1, 10, 17, 25];
 
 [n_samples,header] = load_dataset(FILE_NAME, Cat, true);
 
-
+load_t_begin = cputime;
 % load ('Dataset_historico', 'Data_hist');
 [hist_n_samples,hist_header,hist_video_IDs,...
  hist_titles,hist_channel_names,hist_categories,...
  hist_tags,hist_descriptions] = load_dataset(FILE_NAME_HISTORY, Cat, false);
+load_time = cputime - Load_t_begin;
+fprintf(1,"Dataset carregado em %.3f s",load_time)
 
+NB_t_begin = cputime;
 %
 % Implementação NB
 load("Dataset_reduzido.mat", 'Data');
@@ -27,6 +30,10 @@ flag_perm = true;
 % Carregar dataset
 %
 
+NB_time = cputime - NB_t_begin;
+fprintf(1,"Classificador NB carregado em %.3f s",NB_time)
+
+BF_t_begin = cputime;
 
 % Parametros do BF
 N_SAMPLE_MULTIPLIER = 8;
@@ -88,7 +95,8 @@ fprintf(1,"Vídeos adicionados para recomendação: %d\n",non_watched_videos);
 [recommended_category,views] = calculate_category(hist_categories,hist_n_samples);
 
 fprintf(1,"Categoria recomendada: %s (%d)\n",recommended_category,views);
-
+BF_time = cputime - BF_t_begin;
+fprintf(1,"Categoria calculada  em %.3f s",BF_time)
 %%
 %% Implementação MinHash
 %%
